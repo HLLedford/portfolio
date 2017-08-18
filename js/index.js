c4b44c134b9592b3682bc7c62d94f364e6592341
HLLedford <ledfordhl@email.wofford.edu>
2017-08-18 13:00:46 -0400

    Initial site files.
    Initial site files with more incoming in the next few weeks.

---
+++ js/index.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    
+//    Projects
+    var divStr = "<div>" + "</div>";
+
+    var html = $(divStr);
+
+    html.appendTo($('#projects'))
+    
+//    About Me
+    
+    
+//    Blog
+    
+    
+//    Fetch File
+    // $.get('https://api.github.com/repositories/94455851/contents/content', function(responseText) {
+        // alert(responseText.parse);
+    // });
+    
+    // $.ajax({
+        // url: "https://api.github.com/repositories/94455851/contents/js",
+        // dataType: "json",
+        // success: function (returndata)
+        // {
+        // //$("#result").html(returndata[0]["object"]["sha"]);
+        // alert(returndata[1]["name"]);
+        // }  
+    // });
+});
