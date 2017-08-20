bcc22bb5a03167dbdb7f3e874722838939dad08e
HLLedford <ledfordhl@email.wofford.edu>
2017-08-20 15:35:04 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,30 +1,59 @@
 $(document).ready(function() {
     
 //    Projects
-    var divStr = "<div>" + "</div>";
-
-    var html = $(divStr);
-
-    html.appendTo($('#projects'))
-    
-//    About Me
-    
-    
-//    Blog
-    
-    
-//    Fetch File
-    // $.get('https://api.github.com/repositories/94455851/contents/content', function(responseText) {
-        // alert(responseText.parse);
-    // });
-    
     // $.ajax({
-        // url: "https://api.github.com/repositories/94455851/contents/js",
+        // url: "https://api.github.com/repositories/94455851/contents/CMS/Projects",
         // dataType: "json",
         // success: function (returndata)
         // {
-        // //$("#result").html(returndata[0]["object"]["sha"]);
         // alert(returndata[1]["name"]);
         // }  
     // });
+    
+    // var divStr = "<div>" + "</div>";
+
+    // var html = $(divStr);
+
+    // html.appendTo($('#projects'))
+    
+//    About Me
+    $.ajax({
+        url: "https://api.github.com/repositories/94455851/contents/CMS/About",
+        dataType: "json",
+        success: function (returndata)
+        {
+            fetchHTML(returndata[0]["download_url"], 2);
+        }  
+    });
+    
+    
+//    Blog
+    // $.ajax({
+        // url: "https://api.github.com/repositories/94455851/contents/CMS/Blog",
+        // dataType: "json",
+        // success: function (returndata)
+        // {
+        // alert(returndata[1]["name"]);
+        // }  
+    // });
+    
 });
+
+function fetchHTML(download_url, section) {
+    $.get(download_url, function(responseText) {
+        var html = $(responseText);
+        
+        if (section == 1) {
+            
+        }
+        
+        if (section == 2) {
+            html.appendTo($('#about'));
+        }
+        
+        if (section == 3) {
+            
+        }
+    });
+    
+};
